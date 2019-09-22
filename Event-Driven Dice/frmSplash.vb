d65e8d935c797acd1c2fd5066b7d9a3889cce27a
Luke B <lukebriggs02@gmail.com>
2019-09-22 21:27:12 +0100

    v1.0.0
    Includes a player and a dealer. Each game is self contained with a simple list keeping track of previous games.

---
+++ Event-Driven Dice/frmSplash.vb
@@ -0,0 +1,14 @@
+﻿Public Class frmSplash
+    Private Sub frmSplash_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        tmrSplash.Start()
+        tmrSplash.Interval = 4000
+    End Sub
+
+    Private Sub tmrSplash_Tick(sender As Object, e As EventArgs) Handles tmrSplash.Tick
+        frmDiceGame.Show()
+        Me.Hide()
+        tmrSplash.Stop()
+
+
+    End Sub
+End Class
